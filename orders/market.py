3db2702d539b14ef70203f2103b1d6ce06c8453f
Casey Rock <rock@mac.mynetworksettings.com>
2025-05-17 11:22:42 -0400

    added the a class for the alpaca trading api

--- orders/market.py
+++ orders/market.py
@@ -11,19 +11,59 @@ API_KEY = os.getenv("API_KEY")
 API_SECRET = os.getenv("API_SECRET")
 BASE_URL = os.getenv("https://paper-api.alpaca.markets/v2")
 
-trading_client = TradingClient(API_KEY, API_SECRET, paper=True)
 
-# Define the market order parameters
-market_order_data = MarketOrderRequest(
-    symbol="NVDA",          # Stock symbol
-    qty=1,                 # Number of shares
-    side=OrderSide.BUY,     # Buy or Sell
-    time_in_force=TimeInForce.GTC  # Good Till Cancelled
-)
+class AlpacaTrading():
+    def __init__(self):
+        self.trading_client = TradingClient(API_KEY, API_SECRET, paper=True)
 
-# Submit the market order
-try:
-    market_order = trading_client.submit_order(order_data=market_order_data)
-    print(f"Market order placed: {market_order.id} for {market_order.qty} shares of {market_order.symbol}")
-except Exception as e:
-    print(f"Error placing order: {e}")
+
+    def listCurrentPositions(self):
+        try:
+            positions =self.trading_client.get_all_positions()
+            # Display your holdings
+            print(positions)
+            for position in positions:
+                print(f"Symbol: {position.symbol}")
+                print(f"Quantity: {position.qty}")
+                print(f"Market Value: ${position.market_value}")
+                print(f"Average Entry Price: ${position.avg_entry_price}")
+                print(f"Unrealized P/L: ${position.unrealized_pl}")
+                print("-" * 30)
+                return positions
+        except Exception as e:
+            print(f"Error Listing Positions: {e}")
+
+
+    def submitMarkerOrder(self, symbol,qty=1,time_in_force=TimeInForce.DAY):
+        try:
+            market_order_data=  MarketOrderRequest(
+                symbol=symbol,          # Stock symbol
+                qty=qty,                 # Number of shares
+                side=OrderSide.BUY,     # Buy or Sell
+                time_in_force=time_in_force # Good Till Cancelled
+            )
+            market_order =self.trading_client.submit_order(order_data=market_order_data)
+            print(f"Market order placed: {market_order.id} for {market_order.qty} shares of {market_order.symbol}")
+        except Exception as e:
+            print(f"Error placing order: {e}")
+
+
+    def submitSellOrder(self, symbol,qty=1,time_in_force=TimeInForce.DAY):
+        try:    
+            sell_order_data = MarketOrderRequest(
+                symbol=symbol,               # Replace with your desired stock symbol
+                qty=qty,                       # Number of shares to sell
+                side=OrderSide.SELL,         # Specify the order side as SELL
+                time_in_force=time_in_force  # Order valid for the day
+            )
+            order =self.trading_client.submit_order(order_data=sell_order_data)
+            print(f"Sell order placed: {sell_order_data.id} for {sell_order_data.qty} shares of {sell_order_data.symbol}")
+        except Exception as e:
+            print(f"Error Selling order: {e}")
+
+
+
+trader = AlpacaTrading()
+trader.listCurrentPositions()
+
+
