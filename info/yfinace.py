5da3593a4ef427f48de2c7bfc8359cd326aa519c
Casey Rock <rock@mac.mynetworksettings.com>
2025-05-17 12:07:03 -0400

    created a class for Yahoo Finance

---
+++ info/yfinace.py
@@ -0,0 +1,25 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+
+
+class YahooFinance:
+    def __init__(self):
+        pass
+
+    def getHistoricalStockData(self,symbol,period="6mo",interval="1hr"):
+        ticker = yf.Ticker(symbol)
+        hist = ticker.history(period=period, interval=interval) # Get data for the past month
+        return hist
+    
+    def plotHistoricalStockData(self,hist):
+        # Create the plot
+        plt.figure(figsize=(10, 6))
+        plt.plot(hist.index, hist['Close'], label='AAPL Closing Price', color='blue')
+        plt.title('Apple (AAPL) Stock Closing Prices - Last 1 Year')
+        plt.xlabel('Date')
+        plt.ylabel('Price (USD)')
+        plt.grid(True)
+        plt.legend()
+
+        plt.show()
